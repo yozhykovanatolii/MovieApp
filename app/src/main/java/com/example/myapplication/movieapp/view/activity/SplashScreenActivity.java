369cf9982f6c918d99d91157c539ada055771d53
Yozhykov Anatolii <yozhykovanatolii@gmail.com>
2024-07-24 10:46:12 +0300

    Created UI for Splash Screen and was tested

---
+++ app/src/main/java/com/example/myapplication/movieapp/view/activity/SplashScreenActivity.java
@@ -0,0 +1,44 @@
+package com.example.myapplication.movieapp.view.activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.example.myapplication.movieapp.R;
+import com.example.myapplication.movieapp.viewmodel.AuthViewModel;
+
+import dagger.hilt.android.AndroidEntryPoint;
+
+@AndroidEntryPoint
+public class SplashScreenActivity extends AppCompatActivity {
+    private AuthViewModel authViewModel;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        authViewModel = new ViewModelProvider(this).get(AuthViewModel.class);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        authViewModel.getUserById().observe(this, user -> {
+            if(user == null){
+                SplashScreenActivity.this.usingIntent(MainActivity.class);
+            }else{
+                SplashScreenActivity.this.usingIntent(MainWindowActivity.class);
+            }
+        });
+    }
+
+    private void usingIntent(Class<?> currentClass){
+        new Handler().postDelayed(() -> {
+            Intent intent = new Intent(SplashScreenActivity.this, currentClass);
+            startActivity(intent);
+        }, 2000);
+    }
+}
